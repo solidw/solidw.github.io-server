f0754ec03c264775f519d5d278554652c03f57a0
solidw <solidw@naver.com>
2022-07-31 15:49:47 +0900

    feat: http://localhost:3000 CORS 추가

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://solidw.github.io',
+    origin: ['http://localhost:3000', 'https://solidw.github.io'],
   });
   await app.listen(process.env.PORT ?? 80);
 }
